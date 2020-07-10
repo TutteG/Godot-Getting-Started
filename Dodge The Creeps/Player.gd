f5430bb8dbdd0a4c233ae77ccd319e51da619d5e
Torben-Pop! <torben_grove@live.dk>
2020-07-10 22:06:04 +0200

    My first game - mouse controls and ready for export

--- Dodge The Creeps/Player.gd
+++ Dodge The Creeps/Player.gd
@@ -6,24 +6,31 @@ signal hit
 export var speed = 400
 var screen_size
 
+var target = Vector2()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	screen_size = get_viewport_rect().size
 	hide()
+	screen_size = get_viewport_rect().size
+
+func start(pos):
+	position = pos
+	show()
+	$CollisionShape2D.disabled = false;
+
+func _input(event):
+	if event is InputEventScreenTouch and event.pressed:
+		target = event.position
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	var velocity = Vector2()
 	
-	if Input.is_action_pressed("ui_right"):
-		velocity.x += 1
-	if Input.is_action_pressed("ui_left"):
-		velocity.x -= 1
-	if Input.is_action_pressed("ui_down"):
-		velocity.y += 1
-	if Input.is_action_pressed("ui_up"):
-		velocity.y -= 1
+	if position.distance_to(target) > 10:
+		velocity = target - position
+	
+	
+	
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
 		#$ is shorthand for the get_node() function
@@ -51,8 +58,4 @@ func _on_Player_body_entered(body):
 	$CollisionShape2D.set_deferred("disabled", true)
 
 
-func start(pos):
-	position = pos
-	show()
-	$CollisionShape2D.disabled = false;
-	
+
