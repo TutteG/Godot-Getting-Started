57e7680983ae3a39c97a96aa2448cd13051ce8b2
Torben-Pop! <torben_grove@live.dk>
2020-07-10 20:55:17 +0200

    My first game, HUD included

--- Dodge The Creeps/Main.gd
+++ Dodge The Creeps/Main.gd
@@ -20,12 +20,18 @@ func _ready():
 func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
-
+	$HUD.show_game_over()
+	get_tree().call_group("mobs", "queue_free")
+	$Music.stop()
+	$DeathSound.play()
 
 func new_game():
 	score = 0
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
+	$Music.play()
 
 
 func _on_StartTimer_timeout():
@@ -35,6 +41,7 @@ func _on_StartTimer_timeout():
 
 func _on_ScoreTimer_timeout():
 	score += 1
+	$HUD.update_score(score)
 
 
 func _on_MobTimer_timeout():
