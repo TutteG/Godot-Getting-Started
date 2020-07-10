04d9ebd989f57e48d488ac2418d18c48c62fcac0
Torben Grove <t.grove@epiito.com>
2020-07-10 18:14:12 +0200

    My first game - no HUD

---
+++ Dodge The Creeps/Main.gd
@@ -0,0 +1,54 @@
+extends Node2D
+
+export (PackedScene) var Mob
+var score
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	randomize()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func game_over():
+	$ScoreTimer.stop()
+	$MobTimer.stop()
+
+
+func new_game():
+	score = 0
+	$Player.start($StartPosition.position)
+	$StartTimer.start()
+
+
+func _on_StartTimer_timeout():
+	$MobTimer.start()
+	$ScoreTimer.start()
+
+
+func _on_ScoreTimer_timeout():
+	score += 1
+
+
+func _on_MobTimer_timeout():
+	$MobPath/MobSpawnLocation.offset = randi()
+	
+	var mob = Mob.instance()
+	add_child(mob)
+	
+	var direction = $MobPath/MobSpawnLocation.rotation + PI / 2
+	
+	mob.position = $MobPath/MobSpawnLocation.position
+	
+	direction += rand_range(-PI / 4, PI / 4)
+	mob.rotation = direction
+	
+	mob.linear_velocity = Vector2(rand_range(mob.min_speed, mob.max_speed), 0)
+	mob.linear_velocity = mob.linear_velocity.rotated(direction)
---
+++ Dodge The Creeps/Mob.gd
@@ -0,0 +1,16 @@
+extends RigidBody2D
+
+# Declare member variables here. Examples:
+export var min_speed = 150
+export var max_speed = 250
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var mob_types = $AnimatedSprite.frames.get_animation_names()
+	$AnimatedSprite.animation = mob_types[randi() % mob_types.size()]
+
+func _on_VisibilityNotifier2D_screen_exited():
+	queue_free()
+
+
